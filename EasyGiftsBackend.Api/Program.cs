0d31d17137e08634c38694d5e75c8fa61a123730
Hugo Pat <Hugo.Paternoster@hotmail.com>
2026-01-06 18:29:50 +0100

    Add : Swagger

--- EasyGiftsBackend.Api/Program.cs
+++ EasyGiftsBackend.Api/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using EasyGiftsBackend.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     {
@@ -16,6 +21,12 @@ builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
